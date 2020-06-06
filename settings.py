ce8f8bcc31c1e5224b865b803eaa39ee424e5f61
kintaro85 <59736036+kintaro85@users.noreply.github.com>
2020-06-06 16:13:23 +0100

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        #below are the ship settings
+        self.ship_speed_factor = 1.5
+        #bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 5
+        self.bullet_height = 15
+        self.bullet_color = 250, 0, 0
